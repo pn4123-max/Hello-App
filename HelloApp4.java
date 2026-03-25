1a3a19a9238f846c24660cc956320b45cadacf64
N.Pradhanya <pn4123@srmist.edu.in>
2026-03-25 09:13:11 +0530

    Add files via upload

---
+++ HelloApp4.java
@@ -0,0 +1,18 @@
+public class HelloApp4 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        StringBuilder names = new StringBuilder();
+        for (String name : args) {
+            if (names.length() > 0) {
+                names.append(", ");
+            }
+            names.append(name);
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
