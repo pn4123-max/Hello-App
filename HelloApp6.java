0b328d778e9f3a569c8ae5a2ec6decd0bda2ceee
N.Pradhanya <pn4123@srmist.edu.in>
2026-03-25 09:23:54 +0530

    Add files via upload

---
+++ HelloApp6.java
@@ -0,0 +1,6 @@
+public class HelloApp6 {
+    public static void main(String[] args) {
+        String names = (args.length == 0) ? "World" : String.join(", ", args);
+        System.out.println("Hello, " + names + "!");
+    }
+}
